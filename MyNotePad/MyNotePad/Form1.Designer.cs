2c2550852ad9ffe80c4f6f4eb982093eb8e407b2
Hakimov Bulat <bulathakimov1704@gmail.com>
2024-09-12 19:24:24 +0300

    форма зеленая и нет кнопки закрыть

--- MyNotePad/MyNotePad/Form1.Designer.cs
+++ MyNotePad/MyNotePad/Form1.Designer.cs
@@ -29,26 +29,14 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            button1 = new Button();
             SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new Point(279, 202);
-            button1.Name = "button1";
-            button1.Size = new Size(109, 41);
-            button1.TabIndex = 0;
-            button1.Text = "Закрыть";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(11F, 22F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(255, 128, 0);
-            ClientSize = new Size(1390, 255);
-            Controls.Add(button1);
+            BackColor = Color.FromArgb(128, 255, 128);
+            ClientSize = new Size(527, 258);
             Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Margin = new Padding(4, 3, 4, 3);
@@ -59,7 +47,5 @@
         }
 
         #endregion
-
-        private Button button1;
     }
 }
